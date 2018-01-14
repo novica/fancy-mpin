ee01384444102854ddc8316e457b5d0301708a98
novica <nnovica@gmail.com>
2018-01-14 19:39:30 +0100

    fix table name

--- dbmodel.py
+++ dbmodel.py
@@ -1,10 +1,10 @@
 class PayRecord(db.Model):
-    _tablename='PayRecord'
+    __tablename__='PayRecord'
     id = db.Column(db.Integer, primary_key=True)
     Year = db.Column(db.Integer, nullable=False)
     Month = db.Column(db.Integer, nullable=False)
     Hours = db.Column(db.Integer, nullable=False)
-    CompanyId = db.Column(db.Integer, nullable=False, db.ForeignKey('CompanyRecord.id'))
+    CompanyId = db.Column(db.Integer, db.ForeignKey('CompanyRecord.id'), nullable=False)
     Embg = db.Column(db.String(13),  nullable=False)
     LastName = db.Column(db.String(30),  nullable=False)
     FirstName = db.Column(db.String(30),  nullable=False)
@@ -52,14 +52,14 @@ class PayRecord(db.Model):
     BankAccount = db.Column(db.Ingeter,  nullable=False)
 
 class CompanyRecord(db.Model):
-    _tablename='CompanyRecord'
+    __tablename__='CompanyRecord'
     id = db.Column(db.Integer, primary_key=True)
     ComapnyName = db.Column(db.String(30), nullable=False)
     EDB = db.Column(db.String(13), nullable=False)
     EMBS = db.Column(db.String(7), nullable=False)
 
 class Deductions(db.Model):
-    _tablename='Deductions'
+    __tablename__='Deductions'
     id = db.Column(db.Integer, primary_key=True)
     Year = db.Column(db.Integer, nullable=False)
     TaxDeductions = db.Column(db.Float,  nullable=False)
